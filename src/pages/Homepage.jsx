996d456befc1e69a61c07adfdda60234f15a0e91
Sunyoung Lee <s3u0n4@gmail.com>
2024-12-16 18:20:12 +0900

    feat:Main grid 개발

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,14 @@
+import Header from "../components/layout-page/Header";
+import Main from "../components/layout-page/Main";
+import Footer from "../components/layout-page/Footer";
+
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return ((<>
+    <header><Header/></header>
+    <main><Main/></main>
+    <footer><Footer/></footer>
+  </>))
+
 };
 
 export default Homepage;
--- src/pages/LayoutPage.jsx
+++ src/pages/LayoutPage.jsx
@@ -1,15 +1,11 @@
-import { Outlet } from 'react-router-dom';
-import Header from '../components/layout-page/Header';
-import Footer from '../components/layout-page/Footer';
+import {Outlet} from 'react-router-dom';
 
 const LayoutPage = () => {
   return (
     <>
-      <Header />
       <main>
-        <Outlet />
+        <Outlet/>
       </main>
-      <Footer />
     </>
   );
 };
