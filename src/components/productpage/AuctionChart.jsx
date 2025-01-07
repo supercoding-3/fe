9109feb964af0d4eebe505abed0804b3a98157d4
LJS0000 <leejisu2281@gmail.com>
2025-01-07 23:42:29 +0900

    WIP: 차트 목업 추가

--- src/components/productpage/AuctionChart.jsx
+++ src/components/productpage/AuctionChart.jsx
@@ -1,7 +1,32 @@
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
+
 const AuctionChart = () => {
+  const today = new Date();
+  const dateString = today.toLocaleDateString();
+
+  const mockData = [
+    { date: dateString, price: 100 },
+    { date: dateString, price: 3000 },
+    { date: dateString, price: 2000 },
+    { date: dateString, price: 2780 },
+    { date: dateString, price: 1890 },
+    { date: dateString, price: 500000 },
+  ];
+
   return (
     <div>
-      <div>차트가 들어갑니다</div>
+      <BarChart
+        width={600}
+        height={300}
+        data={mockData}
+        margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" />
+        <YAxis scale="log" domain={[1, 'dataMax']} />
+        <Tooltip />
+        <Bar dataKey="price" fill="#8884d8" />
+      </BarChart>
       <button>입찰</button>
     </div>
   );
