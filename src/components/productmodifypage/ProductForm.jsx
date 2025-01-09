c0bc11c75af93b0ff2781b1823c74907c462df0c
LJS0000 <leejisu2281@gmail.com>
2025-01-10 02:28:55 +0900

    Update: 상품 업로드 페이지 formdata 수정

--- src/components/productmodifypage/ProductForm.jsx
+++ src/components/productmodifypage/ProductForm.jsx
@@ -1,8 +1,12 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import axios from '../../axios/axios';
 import '../../scss/components/productmodifypage/ProductForm.scss';
+import { PRODUCT_CATEGORY } from '../../constants/productCategory';
 
 const ProductForm = ({ productData }) => {
+  const navigate = useNavigate();
+
   const [images, setImages] = useState([]);
   const [previews, setPreviews] = useState([]);
 
@@ -21,14 +25,20 @@ const ProductForm = ({ productData }) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const formData = new FormData(e.target);
-    const imageData = new FormData(images);
-
+    const formData = new FormData();
+    const product = {
+      title: e.target.title.value,
+      description: e.target.description.value,
+      startingBidPrice: e.target.startingBidPrice.value,
+      immediatePrice: e.target.immediatePrice.value,
+      category: e.target.category.value,
+      productEndDate: e.target.productEndDate.value,
+    };
+    formData.append('product', JSON.stringify(product));
+    formData.append('images', images);
     try {
-      const res = await axios.post('/product/register', {
-        product: formData,
-        images: imageData,
-      });
+      const res = await axios.post('/product/register', formData);
+      console.log(res);
     } catch (err) {
       console.error('상품 데이터를 불러오는 중 오류 발생:', err);
     }
@@ -53,18 +63,18 @@ const ProductForm = ({ productData }) => {
         onChange={handleFileChange}
         className="product-form__form__input"
       />
-      <form
-        onSubmit={handleSubmit}
-        encType="multipart/form-data"
-        className="product-form"
-      >
+      <form onSubmit={handleSubmit} className="product-form">
         <label className="product-form__label">
           <span>제목</span>
-          <input type="text" className="product-form__input" />
+          <input id="title" type="text" className="product-form__input" />
         </label>
         <label className="product-form__label">
           <span>입찰 마감 기한</span>
-          <input type="date" className="product-form__input" />
+          <input
+            id="productEndDate"
+            type="date"
+            className="product-form__input"
+          />
         </label>
         <label className="product-form__label">
           <span>경매 시작가</span>
@@ -74,6 +84,7 @@ const ProductForm = ({ productData }) => {
               className="product-form__input-wrapper--checkbox"
             />
             <input
+              id="startingBidPrice"
               type="number"
               className="product-form__input-wrapper--input"
             />
@@ -81,18 +92,27 @@ const ProductForm = ({ productData }) => {
         </label>
         <label className="product-form__label">
           <span>즉시 구매가</span>
-          <input type="number" className="product-form__input" />
+          <input
+            id="immediatePrice"
+            type="number"
+            className="product-form__input"
+          />
         </label>
         <label className="product-form__label">
           <span>카테고리</span>
-          <select className="product-form__input">
-            <option>1</option>
-            <option>2</option>
+          <select id="category" className="product-form__input">
+            {PRODUCT_CATEGORY.map((category, i) => (
+              <option key={i}>{category}</option>
+            ))}
           </select>
         </label>
         <label className="product-form__label">
           <span>설명</span>
-          <textarea rows="10" className="product-form__input" />
+          <textarea
+            id="description"
+            rows="10"
+            className="product-form__input"
+          />
         </label>
         <button type="submit" className="product-form__button">
           {productData ? '수정' : '등록'}
