13c9b4e83c795db85e1bfa5ec7bccedac0bfdcda
LJS0000 <leejisu2281@gmail.com>
2025-01-10 01:09:42 +0900

    Fix: 라우터 휴먼에러 및 라우팅 주소 수정

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import PrivateRoute from './PrivateRoute';
 // pages
 import LayoutPage from '../pages/LayoutPage';
@@ -10,12 +11,10 @@ import ChatListPage from '../pages/ChatListPage';
 import ProfilePage from '../pages/ProfilePage';
 import NotFoundPage from '../pages/NotFoundPage';
 import AuthPage from '../pages/AuthPage';
-import { useSelector } from 'react-redux';
 import ProfileEdit from '../components/profilepage/ProfileEdit';
 
 const Router = () => {
-  // TODO: useSelector로 실제 값 가져오기
-  const isLogin = useSelector((state) => state.user);
+  const isLogin = useSelector((state) => state.user.isLogin);
 
   return (
     <BrowserRouter>
@@ -26,9 +25,10 @@ const Router = () => {
           <Route path="/signup" element={<AuthPage />} />
           <Route path="/product/:id" element={<ProductPage />} />
           <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/edit" element={<ProfileEdit />} />
           <Route element={<PrivateRoute isLogin={isLogin} />}>
-            <Route path="/product/modify" element={<ProductModifyPage />} />
+            <Route path="/profile/edit" element={<ProfileEdit />} />
+            <Route path="/product/create" element={<ProductModifyPage />} />
+            <Route path="/product/edit" element={<ProductModifyPage />} />
             <Route path="/chat" element={<ChatListPage />} />
             <Route path="/chat/:id" element={<ChatPage />} />
           </Route>
