f5938de0147f976fb21122436f24e2a5da17da27
Sunyoung Lee <s3u0n4@gmail.com>
2025-01-21 19:05:59 +0900

    Fix: 이미지 수정

--- src/components/homepage/MainGrid.jsx
+++ src/components/homepage/MainGrid.jsx
@@ -1,27 +1,18 @@
 import '../../scss/components/homepage/MainGrid.scss';
+import {Link} from "react-router-dom";
 
-const MainGrid = ({ items }) => {
-  return (
-    <div className="grid">
-      {items.map((item) => (
-        <div key={item.id} className="grid__card">
-          {item.image ? (
-            <img src={item.image} alt={item.title} className="grid__image" />
-          ) : (
-            <div className="grid__placeholder"></div>
-          )}
-          <div className="grid__info">
-            <h3>
-              {item.title.length > 30
-                ? `${item.title.slice(0, 30)}...`
-                : item.title}
-            </h3>
-            <p>{item.price}</p>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+const MainGrid = ({items}) => {
+  return (<div className="grid">
+    {items.map((item) => (<div key={item.id} className="grid__card">
+      {item.img ? (<img src={item.img} alt={item.title} className="grid__image"/>) : (
+        <div className="grid__placeholder"></div>)}
+      <div className="grid__info">
+        <Link to={`/product/${item.id}`}><h3>
+          {item.title.length > 30 ? `${item.title.slice(0, 30)}...` : item.title}
+        </h3></Link>
+        <p>{item.price}</p>
+      </div>
+    </div>))}
+  </div>);
 };
-
 export default MainGrid;
