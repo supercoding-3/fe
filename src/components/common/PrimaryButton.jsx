b97c715c80f9835604e1866d2ff6d3b84bbc1a3d
LJS0000 <leejisu2281@gmail.com>
2025-01-21 22:41:31 +0900

    Add: 공용 버튼 컴포넌트 테마 추가

--- src/components/common/PrimaryButton.jsx
+++ src/components/common/PrimaryButton.jsx
@@ -1,8 +1,27 @@
 import '../../scss/components/common/PrimaryButton.scss';
 
-const PrimaryButton = ({ type, buttonName, isFull = false }) => {
+/**
+ * 공용 버튼 컴포넌트
+ * @param {string} type
+ * @param {string} buttonName
+ * @param {function} onClick
+ * @param {boolean} isFull
+ * @param {'primary' | 'success' | 'cancel' | 'disabled'} theme
+ */
+const PrimaryButton = ({
+  type,
+  buttonName,
+  onClick,
+  isFull = false,
+  theme = 'primary',
+}) => {
   return (
-    <button type={type} className={`primary-button ${isFull ? 'is-full' : ''}`}>
+    <button
+      type={type}
+      className={`primary-button ${isFull ? 'is-full' : ''} ${theme}`}
+      onClick={onClick}
+      disabled={theme === 'disabled'}
+    >
       {buttonName}
     </button>
   );
