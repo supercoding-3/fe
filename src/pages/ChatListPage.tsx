3774741dbede53717cf05169b28d0bbbb6df02cc
LJS0000 <leejisu2281@gmail.com>
2025-01-30 17:39:36 +0900

    WIP: TS 마이그레이션_Chat 데이터

--- src/pages/ChatListPage.tsx
+++ src/pages/ChatListPage.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from '../axios/axios';
+import { ChatData } from 'types/Chat';
 import ChatListCard from '../components/chatlistpage/ChatListCard';
 import EmptyState from '../components/common/EmptyState';
 
 const ChatListPage = () => {
-  const [chatList, setChatList] = useState([]);
+  const [chatList, setChatList] = useState<ChatData[]>([]);
 
   const fetchChatList = async () => {
     try {
