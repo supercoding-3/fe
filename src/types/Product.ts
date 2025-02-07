9200fb7be720b94dcf7627f229aaa4dde155293f
LJS0000 <leejisu2281@gmail.com>
2025-02-07 18:22:19 +0900

    WIP: TS 마이그레이션_상품 생성

---
+++ src/types/Product.ts
@@ -0,0 +1,9 @@
+export interface ProductData {
+  productId?: number;
+  title: string;
+  description: string;
+  startingBidPrice?: number;
+  immediatePrice: number;
+  category: string;
+  productEndDate?: string;
+}
