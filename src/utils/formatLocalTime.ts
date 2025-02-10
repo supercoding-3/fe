98cd95aacb1cb15bd81ffd9b1f5cbadb613b94e8
LJS0000 <leejisu2281@gmail.com>
2025-02-10 17:08:20 +0900

    WIP: TS 마이그레이션_유틸리티 함수

--- src/utils/formatLocalTime.ts
+++ src/utils/formatLocalTime.ts
@@ -1,4 +1,9 @@
-export const formatLocalTime = (isoString) => {
+/**
+ * 한국 표준 시간 형식으로 변환
+ * @param isoString
+ * @returns 1999-12-31 23:59:59
+ */
+export const formatLocalTime = (isoString: string) => {
   const date = new Date(isoString);
 
   return date.toLocaleString('ko-KR', {
