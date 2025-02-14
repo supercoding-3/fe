f5856af9a923fa4d26aaf020cd8f8f5172324e4a
LJS0000 <leejisu2281@gmail.com>
2025-02-14 20:32:59 +0900

    Fix: 로그인 상태 유지 로직 수정

--- src/redux/reducers/user.ts
+++ src/redux/reducers/user.ts
@@ -1,10 +1,20 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from '@/axios/axios';
 import { UserState } from '@/types/User';
 
 const initialState: UserState = {
   isLogin: false,
   user: null,
 };
+
+export const __fetchUser = createAsyncThunk('fetchUser', async (_, api) => {
+  try {
+    const resp = await axios.get('/auth/me');
+    return resp.data;
+  } catch (error) {
+    return api.rejectWithValue('Unauthorized');
+  }
+});
 
 export const userSlice = createSlice({
   name: 'user',
@@ -18,6 +28,16 @@ export const userSlice = createSlice({
       state.isLogin = false;
       state.user = null;
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(__fetchUser.fulfilled, (state, action) => {
+      state.isLogin = true;
+      state.user = action.payload;
+    });
+    builder.addCase(__fetchUser.rejected, (state) => {
+      state.isLogin = false;
+      state.user = null;
+    });
   },
 });
 
