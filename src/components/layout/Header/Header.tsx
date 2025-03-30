dbc1636eae968fd7eb93b3dc55fc22e45e2b54fa
LJS0000 <leejisu2281@gmail.com>
2025-03-30 21:53:03 +0900

    UI: 헤더에 버튼 제거

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,4 +1,3 @@
-import { MdMenu } from 'react-icons/md';
 import { Input } from '@/components/ui';
 import './header.scss';
 
@@ -8,9 +7,6 @@ const Header = () => {
       <div className="header__input">
         <Input type="text" placeholder="검색어를 입력하세요" />
       </div>
-      <button className="header__menu-button">
-        <MdMenu />
-      </button>
     </div>
   );
 };
