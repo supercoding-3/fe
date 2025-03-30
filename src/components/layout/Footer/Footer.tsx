c1ba979fe40d4f5affcb90fa0ff73041bd69a632
LJS0000 <leejisu2281@gmail.com>
2025-03-31 04:16:02 +0900

    UI: 활성화된 메뉴 색상 변경

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -1,14 +1,19 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { NAV_MENU } from '@/constants/navMenu';
 import './footer.scss';
 
 const Footer = () => {
+  const location = useLocation();
+
   return (
     <nav className="nav">
       <ul className="menu">
         {NAV_MENU.map((menu) => (
           <li key={menu.id}>
-            <Link to={menu.path} className="menu__link">
+            <Link
+              to={menu.path}
+              className={`menu__link ${location.pathname === menu.path ? 'menu__link--active' : ''}`}
+            >
               <menu.icon />
               <p> {menu.name}</p>
             </Link>
